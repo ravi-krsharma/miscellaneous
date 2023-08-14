e8fe770b2eda52771aa8b9d5b466ed932d1dc9f3
ravi-krsharma <108462238+ravi-krsharma@users.noreply.github.com>
2023-08-14 14:39:11 +0530

    Updated dash uploader example

--- dash/dash_uploader_example.py
+++ dash/dash_uploader_example.py
@@ -1,15 +1,12 @@
 from pathlib import Path
 import uuid
-
 import dash_uploader as du
 import dash
-# import dash_html_components as html
-# from dash.dependencies import Input, Output, State
 from dash import dcc, html, Input, Output, State, callback_context, ctx
 
 app = dash.Dash(__name__)
 
-count = 0
+
 
 UPLOAD_FOLDER_ROOT = r"C:\tmp\Uploads"
 du.configure_upload(app, UPLOAD_FOLDER_ROOT)
@@ -18,13 +15,14 @@ def get_upload_component(id):
     return du.Upload(
         id=id,
         text="Drag-and-drop file here",
-        text_completed="",
+        text_completed="Upload completed for: ",
         cancel_button=True,
         disabled=False,
         max_file_size=10000,  # in Mb
         upload_id="Electromagnetics",  # Unique session id
         default_style={"minHeight": "None", "lineHeight": "None"},
         max_files=10,
+        text_disabled="The uploader is disabled",
     )
 
 
@@ -59,24 +57,20 @@ app.layout = get_app_layout
 
 
 @du.callback(
-    output=Output("callback-output", "children"),
+    output=Output("dash-uploader", "text"),
     id="dash-uploader",
 )
 def get_a_list(uploadedFiles):
-    # count +=1
-    global count
-    count+=1
-    print("\nget_a_list", uploadedFiles.uploaded_files)
-    # print("\ncount", count)
-    data = []
-    # if filenames:
-    #     with open("filenames.txt", "a") as f:
-    #         f.write(str(filenames))
-    #     data.append(filenames)
-    # else:
-    #     data = None
-    # return dcc.Store(id={"type": "upload-aedt", "index": "upload"}, data=data)
-    return str(uploadedFiles.uploaded_files)
+
+    #uploadedFiles.uploaded_files gives a list of Path objects containing the full path to the uploaded files
+    list_of_files = uploadedFiles.uploaded_files
+    file_names = []
+    
+    if list_of_files:
+        for files in list_of_files:
+            file_names.append(str(files).split("\\")[-1])
+            
+    return f"Upload successful for: {str(file_names)}"
 
 
 
